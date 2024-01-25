885f75c3a7f268ec86ca2331918d4c4dbb7c85f3
Aaron <84269836+Brownyhunter@users.noreply.github.com>
2024-01-25 17:38:11 +0100

    .

--- inspect_data.py
+++ inspect_data.py
@@ -2,4 +2,14 @@ import pandas as pd
 
 tweets = pd.read_csv('tweets_prep_sentiment.csv', sep=',',parse_dates=['created_at'])
 
-print(tweets)
+tweets['sentiment'] = tweets['pos'] - tweets['neg']
+
+tweets['neg_sentiment'] = tweets['sentiment'] < -0.7
+tweets['pos_sentiment'] = tweets['sentiment'] > 0.7
+
+tweets_edges = tweets['neg_sentiment', 'pos_sentiment']
+print(tweets_edges)
+
+
+
+#tweets.to_csv('tweets_prep2.csv', encoding='utf-8',index=False, sep=',')
